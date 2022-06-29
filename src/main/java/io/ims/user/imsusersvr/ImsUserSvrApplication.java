04f6af77e83587078c3bd259ce95a59cbd62a37b
roseutz <roseoutz@gmail.com>
2022-06-29 16:54:13 +0900

    ims 프로젝트 셋팅 작업중

---
+++ src/main/java/io/ims/user/imsusersvr/ImsUserSvrApplication.java
@@ -0,0 +1,13 @@
+package io.ims.user.imsusersvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsUserSvrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImsUserSvrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ims/user/imsusersvr/ImsUserSvrApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ims.user.imsusersvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsUserSvrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
